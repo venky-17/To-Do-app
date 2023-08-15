dd6093620416dc87f6aef4eddf7c9ff2ef2a77c2
Venkatesh K <128626606+venky-17@users.noreply.github.com>
2023-08-15 23:28:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const taskInput = document.querySelector('.inputTask')
+const addBtn = document.querySelector('.addTask')
+const taskList = document.querySelector('.listItems')
+
+function addTask(){
+if(taskInput.value === ''){
+    alert("pls enter something")
+} else{
+    let listItem = document.createElement('li')
+listItem.innerHTML = taskInput.value ;
+
+taskList.appendChild(listItem)
+
+let spanElement = document.createElement('span')
+spanElement.innerHTML= '✘'
+listItem.appendChild(spanElement)
+
+}
+taskInput.value='';
+saveData()
+}
+
+function markChecked(e){
+    if(e.target.tagName ===  'LI'){
+        e.target.classList.toggle('checked');
+        saveData()
+    } else if(e.target.tagName=== 'SPAN'){
+        e.target.parentElement.remove()
+        saveData()
+    } 
+    console.log('dbbjbdj');
+}
+
+function saveData(){
+    localStorage.setItem('data', taskList.innerHTML)
+}
+function getData(){
+    taskList.innerHTML = localStorage.getItem('data')
+}
+getData()
+taskList.addEventListener('click', markChecked)
+addBtn.addEventListener('click', addTask)
